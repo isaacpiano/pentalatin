6932a7aede9c1b13ed040921ef455ae24fd7d0ec
isaacpiano <osnmundo@gmail.com>
2025-06-23 23:21:46 -0500

    Add files via upload

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyChArhb2l1F31036Wcv8YaiR1ymWBashZg",
+  authDomain: "pentalatin.firebaseapp.com",
+  projectId: "pentalatin",
+  storageBucket: "pentalatin.firebasestorage.app",
+  messagingSenderId: "292133961951",
+  appId: "1:292133961951:web:2d448c202f657e81f4696a",
+  measurementId: "G-304603LPFL"
+};
